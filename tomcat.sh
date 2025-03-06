21f7aa7ccd36d1752655c1869e6ae0445e89be7b
upendrayadav1abc <153957985+upendrayadav1abc@users.noreply.github.com>
2025-03-07 01:06:39 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,25 +1,8 @@
-#amazon-linux-extras install java-openjdk11 -y
-#wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.89/bin/apache-tomcat-9.0.89.tar.gz
-#wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.97/bin/apache-tomcat-9.0.97.tar.gz
-
-#tar -zxvf apache-tomcat-9.0.97.tar.gz
-#sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.97/conf/tomcat-users.xml
-#sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-9.0.97/conf/tomcat-users.xml
-#sed -i '58  a\<user username="tomcat" password="raham123" roles="manager-gui, manager-script"/>' apache-tomcat-9.0.97/conf/tomcat-users.xml
-#sed -i '59  a\</tomcat-users>' apache-tomcat-9.0.97/conf/tomcat-users.xml
-#sed -i '56d' apache-tomcat-9.0.97/conf/tomcat-users.xml
-#sed -i '21d' apache-tomcat-9.0.97/webapps/manager/META-INF/context.xml
-#sed -i '22d'  apache-tomcat-9.0.97/webapps/manager/META-INF/context.xml
-#sh apache-tomcat-9.0.97/bin/startup.sh
-
-
-
-wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.97/bin/apache-tomcat-9.0.97.tar.gz
-tar -zxvf apache-tomcat-9.0.97.tar.gz
-sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.97/conf/tomcat-users.xml
-sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-9.0.97/conf/tomcat-users.xml
-sed -i '58  a\<user username="tomcat" password="raham123" roles="manager-gui, manager-script"/>' apache-tomcat-9.0.97/conf/tomcat-users.xml
-sed -i '21d' apache-tomcat-9.0.97/webapps/manager/META-INF/context.xml
-sed -i '22d'  apache-tomcat-9.0.97/webapps/manager/META-INF/context.xml
-sh apache-tomcat-9.0.97/bin/startup.sh
-amazon-linux-extras intall java-openjdk11 -y
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.102/bin/apache-tomcat-9.0.102.tar.gz
+tar -zxvf apache-tomcat-9.0.102.tar.gz
+sed -i '55  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.102/conf/tomcat-users.xml
+sed -i '56  a\<role rolename="manager-script"/>' apache-tomcat-9.0.102/conf/tomcat-users.xml
+sed -i '57  a\<user username="tomcat" password="raham123" roles="manager-gui, manager-script"/>' apache-tomcat-9.0.102/conf/tomcat-users.xml
+sed -i '21d' apache-tomcat-9.0.102/webapps/manager/META-INF/context.xml
+sed -i '22d'  apache-tomcat-9.0.102/webapps/manager/META-INF/context.xml
+sh apache-tomcat-9.0.102/bin/startup.sh
