ed0169ee9507fe3c39480e430776038b39cd439b
upendrayadav1abc <153957985+upendrayadav1abc@users.noreply.github.com>
2025-05-15 17:57:21 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,9 +1,9 @@
 #wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.102/bin/apache-tomcat-9.0.102.tar.gz
 wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.105/bin/apache-tomcat-9.0.105.tar.gz
 tar -zxvf apache-tomcat-9.0.105.tar.gz
-sed -i '55  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.105/conf/tomcat-users.xml
-sed -i '56  a\<role rolename="manager-script"/>' apache-tomcat-9.0.105/conf/tomcat-users.xml
-sed -i '57  a\<user username="tomcat" password="raham123" roles="manager-gui, manager-script"/>' apache-tomcat-9.0.102/conf/tomcat-users.xml
+sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.105/conf/tomcat-users.xml
+sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-9.0.105/conf/tomcat-users.xml
+sed -i '58  a\<user username="tomcat" password="raham123" roles="manager-gui, manager-script"/>' apache-tomcat-9.0.102/conf/tomcat-users.xml
 sed -i '21d' apache-tomcat-9.0.105/webapps/manager/META-INF/context.xml
 sed -i '22d'  apache-tomcat-9.0.105/webapps/manager/META-INF/context.xml
 sh apache-tomcat-9.0.105/bin/startup.sh
